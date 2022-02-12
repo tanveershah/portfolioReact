69e053db85584377eaee7594634cfac88e44c5ac
tanveershah <tanvirhussein@gmail.com>
2022-02-12 18:33:20 -0500

    Update: About page updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,40 @@ const About = () => {
         <section className="card">
           <h2 className="heading">About</h2>
           <article className="cards">
+            <h3>Experience</h3>
+            <img src="./images/experience.jpg" alt="" />
+            <p>
+              I worked as an Instructional Associate and Teaching Fellow at
+              Fullstack Academy NY for their university web development
+              program.As an instructional associate I implemented agile
+              methodologies to provide ongoing technical and project management
+              guidance to students learning full stack JavaScript software
+              development. In addition, I lead group code reviews and stand ups,
+              and conducted technical admissions interviews for prospective
+              students.
+            </p>
+            <p>
+              <strong>Experience outside USA</strong>
+            </p>
+            <p>
+              I worked as an assistant professor in the field of education
+              before moving to the United States.
+            </p>
+          </article>
+          <article className="cards">
+            <h3>Education</h3>
+            <img src="./images/education.jpg" alt="" />
+            <p>Software Engineering Immersive, Fullstack Academy, NY</p>
+
+            <p>Doctor of Education, Dowling College, Long Island, NY</p>
+
+            <p>Master degree in HRM</p>
+            <p>
+              A 64-hour course in American Culture and English, Hunter College
+              Manhattan New York City USA
+            </p>
+          </article>
+          <article className="cards">
             <h3>Travel</h3>
             <img src="./images/travel.jpg" alt="" />
             <div>
@@ -34,41 +68,7 @@ const About = () => {
               pop, country and indian classic.
             </p>
           </article>
-          <article className="cards">
-            <h3>Experience</h3>
-            <img src="./images/experience.jpg" alt="" />
-            <p>
-              I worked as an Instructional Associate and Teaching Fellow at
-              Fullstack Academy NY for their university web development
-              program.As an instructional associate I implemented agile
-              methodologies to provide ongoing technical and project management
-              guidance to students learning full stack JavaScript software
-              development. In addition, I lead group code reviews and stand ups,
-              and conducted technical admissions interviews for prospective
-              students.
-            </p>
 
-            <p>
-              <strong>Experience outside USA</strong>
-            </p>
-            <p>
-              I worked as an assistant professor in the field of education
-              before moving to the United States.
-            </p>
-          </article>
-          <article className="cards">
-            <h3>Education</h3>
-            <img src="./images/education.jpg" alt="" />
-            <p>Software Engineering Immersive, Fullstack Academy, NY</p>
-
-            <p>Doctor of Education, Dowling College, Long Island, NY</p>
-
-            <p>Master degree in HRM</p>
-            <p>
-              A 64-hour course in American Culture and English, Hunter College
-              Manhattan New York City USA
-            </p>
-          </article>
           <article className="cards">
             <h3>Portfolio and Home pages Photos</h3>
             <img src="./images/homeImage.jpg" alt="" />
